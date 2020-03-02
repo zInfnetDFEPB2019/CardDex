8891f0b3c0b02742e270146326283ea33d537977
lucaslb767 <lucaslbarrozo@gmail.com>
2020-03-01 23:02:26 -0300

    cria bola da pokedex

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -16,11 +16,15 @@ export default {
 </script>
 
 <style>
+
+html{
+  min-height: 100vh;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 80px;
 }
+
 </style>
