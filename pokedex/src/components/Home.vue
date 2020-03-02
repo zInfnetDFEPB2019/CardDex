0924126e6bb19720a9565088df28ff435f504168
lucaslb767 <lucaslbarrozo@gmail.com>
2020-03-02 04:25:22 -0300

    commit test

--- pokedex/src/components/Home.vue
+++ pokedex/src/components/Home.vue
@@ -7,10 +7,11 @@
         <li class="sideMenuItem"><a href="">Meu Time</a></li>
       </ul>
       <div class="details">
-
+        <div id="detailsRow1"><p class="detailsText" id="details1"></p><p class="detailsText" id="details2"></p></div>
+        <div id=" detailsRow2"><p class="detailsText" id="details3"></p><p class="detailsText" id="details4"></p></div>
       </div>
       <div class="information">
-        
+
       </div>
   </div>
 </template>
@@ -38,5 +39,18 @@ export default {
     background-color: rgba(0, 0, 0, .4);
     width: 2%;
     height: 100%;
+  }
+
+  .details {
+    background-color: gray;
+    
+
+  }
+
+  .detailsText {
+    padding: 2px;
+    min-width: 200px;
+    min-height: 50px;
+    border-radius: 10px;
   }
 </style>
